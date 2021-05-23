b7ce09c970563fda3b18407b1b51318e2da16c7c
Kyle <kabrah5@students.towson.edu>
2021-05-23 07:55:00 -0400

    reload issue

--- index.js
+++ index.js
@@ -60,14 +60,14 @@ if (process.env.NODE_ENV === "production") {
 
 //to send html page from server to client in case of reload so client
 //can render properly
-if (process.env.NODE_ENV === "production") {
-  const p = window.performance.getEntriesByType("navigation")[0].type;
-  if (p == "reload") {
-    app.use(express.static("client/build"));
+if (
+  process.env.NODE_ENV === "production" &&
+  window.performance.getEntriesByType("navigation")[0].type == "reload"
+) {
+  app.use(express.static("client/build"));
 
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-  }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 app.listen(port, () => console.log(`Server started at port: ${port}`));
