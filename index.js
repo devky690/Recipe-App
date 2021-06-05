f097d4a1bcb9723089165441e49188d468e465b9
Kyle <kabrah5@students.towson.edu>
2021-06-05 18:04:42 -0400

    update crud operation

--- index.js
+++ index.js
@@ -44,8 +44,7 @@ app.use("/category", categoryRouter);
 app.use("/token", tokenRouter);
 
 //if we have port defined or if we dont then run locally at
-//port 8080
-
+//port 8080...CHANGE TO port 8080
 const port = process.env.PORT || 8080;
 
 //serve static assets (client code) if in production
